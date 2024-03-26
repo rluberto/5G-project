adb28162faedc6803e8999d705ae4bb212b586b3
Ray Luberto <rluberto15@gmail.com>
2024-03-26 14:54:18 -0400

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,19 +1,32 @@
 import socket
 import random
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 1002))
-server.listen()
+try:
+    while True:
+        # Establish the server and allow for client connections
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1', 1002))
+        server.listen()
+        client_socket, client_address = server.accept()
 
-client_socket, client_address = server.accept()
+        # Create a new file to write the image to
+        file = open('media/'+str(random.randint(100000, 999999))+'_image.jpg', "wb")
 
+        # Write all of the data received from the client to the image file
+        image_chunk = client_socket.recv(2048)
+        while image_chunk:
+            file.write(image_chunk)
+            image_chunk = client_socket.recv(2048)
+            print(image_chunk.__len__())
+        # ---- record recvd time
+        file.close()
+        client_socket.close()
 
-file = open('media/'+str(random.randint(100000, 999999))+'_image.jpg', "wb")
-image_chunk = client_socket.recv(2048)
+        # ---- get sent time from client
+        # ---- calc elapsed time (latency) recvd - sent
+        # ---- calc bandwidth recvd_file_size/latency
+        # ---- store results
 
-while image_chunk:
-    file.write(image_chunk)
-    image_chunk = client_socket.recv(2048)
-
-file.close()
-client_socket.close()
+# Exit the program if the keyboard interrupt is datected
+except KeyboardInterrupt:
+    exit()
