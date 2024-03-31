edf42bb534497017cbbad4833fd5a33c33b2f190
Ray Luberto <rluberto15@gmail.com>
2024-03-31 15:58:25 -0400

    update project

--- client/client.py
+++ client/client.py
@@ -1,40 +1,33 @@
 import socket
 import time
-import os
-from csv import writer
+import pickle
 
 # Establish a socket connection
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 1002))
+client.connect(('127.0.0.1', 8000))
 
 # Open the image file that is going to be transferred
-file_path = 'media/image.JPG'
+file_path = 'media/image2.jpg'
 file = open(file_path, 'rb')
+
+# Create a send time array
+send_time_array = []
 
 # Write all of the image data to the server
 image_data = file.read(2048)
-start_time = time.time() # Record the start time (used to calculate latench and bandwidth)
 while image_data:
+    send_time_array.append(time.time())
     client.send(image_data)
     image_data = file.read(2048)
-stop_time = time.time() # Record the stop time (used to calculate latench and bandwidth)
-# ---- record sent time
 file.close()
 client.close()
 
-# ---- send sent time
 
-# Collect latency and bandwidth benchmark data
-latency = stop_time - start_time
-file_size = os.path.getsize(file_path)
-bandwidth = file_size/latency
-print("Latency:", latency, "seconds")
-print("Bandwidth:", bandwidth, "bytes/second")
+# Establish a socket connection
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 9000))
 
-# Append latency and bandwidth benchmark data to a csv file
-data_file_name = 'local_benchmarks.csv'
-csv_append_data = [latency, bandwidth]
-with open(data_file_name, 'a') as f_object:
-    writer_object = writer(f_object)
-    writer_object.writerow(csv_append_data)
-    f_object.close()
+data = pickle.dumps(send_time_array)
+client.sendall(data)
+
+client.close()
--- server/server.py
+++ server/server.py
@@ -1,32 +1,88 @@
 import socket
+import threading
 import random
+import pickle
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import json
 
-try:
-    while True:
-        # Establish the server and allow for client connections
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(('127.0.0.1', 1002))
-        server.listen()
-        client_socket, client_address = server.accept()
+receive_time_array = []
+sent_time_array = []
+rta_processing_done = False
+sta_processing_done = False
+file_random_number = ''
 
-        # Create a new file to write the image to
-        file = open('media/'+str(random.randint(100000, 999999))+'_image.jpg', "wb")
+def handle_connection(client_socket, port):
+    global receive_time_array
+    global sent_time_array
+    global rta_processing_done
+    global sta_processing_done
+    global file_random_number
 
-        # Write all of the data received from the client to the image file
+    # Transfer the image from the client to the server
+    if port == 8000: # Used for receiving images
+        print("Received connection on port 8000")
+        file_random_number = str(random.randint(100000, 999999))
+        file = open('media/'+file_random_number+'_media.jpg', "wb")
+        receive_time_array.clear()
         image_chunk = client_socket.recv(2048)
+        receive_time_array.append(time.time())
         while image_chunk:
             file.write(image_chunk)
             image_chunk = client_socket.recv(2048)
-            print(image_chunk.__len__())
-        # ---- record recvd time
+            if image_chunk.__len__() != 0:
+                receive_time_array.append(time.time())
         file.close()
-        client_socket.close()
+        print('RTA: ' + str(len(receive_time_array)))
+        rta_processing_done = True
 
-        # ---- get sent time from client
-        # ---- calc elapsed time (latency) recvd - sent
-        # ---- calc bandwidth recvd_file_size/latency
-        # ---- store results
+    # Transfer the send time data from the client to the server
+    elif port == 9000: # Used for getting benchmark data
+        print("Received connection on port 9000")
+        sent_time_array.clear()
+        data = b""
+        while True:
+            packet = client_socket.recv(4096)
+            if not packet: break
+            data += packet
+        sent_time_array = pickle.loads(data)
+        print('STA: ' + str(len(sent_time_array)))
+        sta_processing_done = True
 
-# Exit the program if the keyboard interrupt is datected
-except KeyboardInterrupt:
-    exit()
+    # Perform calculations based on the send time and receive time data
+    if rta_processing_done and sta_processing_done: # Calculate latency after both send and received arrays are complete
+        print("Latency Array: ")
+        latency_array = []
+        for i in range(len(receive_time_array)):
+            latency = receive_time_array[i] - sent_time_array[i]
+            latency_array.append(latency)
+        print(latency_array)
+        # Print the benchmark arrays to a json file
+        benchmark_data = {
+            "sent_time_array": sent_time_array,
+            "receive_time_array": receive_time_array,
+            "latency_array": latency_array
+        }
+        with open('benchmark-data/'+file_random_number+'_benchmark.json', 'w') as json_file:
+            json.dump(benchmark_data, json_file)
+        rta_processing_done = False
+        sta_processing_done = False
+
+    client_socket.close()
+
+
+def start_server(port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', port))
+    server_socket.listen(5)
+    print(f"Listening on port {port}...")
+
+    while True:
+        client_socket, address = server_socket.accept()
+        print(f"Accepted connection from {address[0]}:{address[1]} on port {port}")
+        threading.Thread(target=handle_connection, args=(client_socket, port)).start()
+
+# Start servers on multiple ports
+threading.Thread(target=start_server, args=(8000,)).start()
+threading.Thread(target=start_server, args=(9000,)).start()
