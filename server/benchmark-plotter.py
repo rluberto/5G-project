9fb1b5c519fd438d92bfe6e35ba3135866b16b70
Ray Luberto <rluberto15@gmail.com>
2024-04-02 11:40:44 -0400

    update plots

--- server/benchmark-plotter.py
+++ server/benchmark-plotter.py
@@ -13,6 +13,7 @@ sent_time_array = data['sent_time_array']
 receive_time_array = data['receive_time_array']
 latency_array = data['latency_array']
 bandwidth_array = data['bandwidth_array']
+transferred_data_length_array = data['transferred_data_length_array']
 
 xpoints = np.array(sent_time_array)
 ypoints = np.array(latency_array)
@@ -30,4 +31,17 @@ plt.xlabel('Bytes Sent (x 2048)')
 plt.ylabel('Bandwidth (Bits/Second)')
 plt.title('Bandwidth vs Bytes Sent')
 plt.plot(ypoints*8)
+plt.show()
+
+total_bytes_transferred_array = []
+for i in range(len(transferred_data_length_array)):
+    total_bytes_transferred_array.append(sum(transferred_data_length_array[:i+1]))
+
+xpoints = np.array(receive_time_array)
+ypoints = np.array(total_bytes_transferred_array)
+
+plt.ylabel('Total Bytes Transferred')
+plt.xlabel('Receive Time (Milliseconds)')
+plt.title('Total Bytes Transferred vs Receive Time')
+plt.plot((xpoints-receive_time_array[0])*1000, ypoints, marker='o')
 plt.show()
