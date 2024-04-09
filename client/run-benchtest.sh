0ae86e0e718139d56830396ced9ce341037bef03
Ray Luberto <rluberto15@gmail.com>
2024-04-09 13:59:33 -0400

    accpt password in bash script

--- client/run-benchtest.sh
+++ client/run-benchtest.sh
@@ -7,7 +7,8 @@ remote_server_python_script=Developer/5G-project/server/server.py
 local_client_transfer_file_path=media/video.mp4
 local_client_python_script=client/client.py
 
+read -s -p "Enter SSH password: " ssh_password
 
-ssh -t $remote_server_user@$remote_server_ip "python3 $remote_python_script $remote_server_ip $local_client_transfer_file_path"
+sshpass -p "$ssh_password" ssh $remote_server_user@$remote_server_ip "python3 $remote_python_script $remote_server_ip $local_client_transfer_file_path"
 
 python3 $local_client_python_script "$remote_server_ip $local_client_transfer_file_path"
