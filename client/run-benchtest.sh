ac393ad4336ab781c247c64554e0a54c0e0c1d2b
Ray Luberto <rluberto15@gmail.com>
2024-04-09 13:44:15 -0400

    benchtest bash script

---
+++ client/run-benchtest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+remote_server_ip=
+remote_server_user=
+remote_server_python_script=Developer/5G-project/server/server.py
+
+local_client_transfer_file_path=media/video.mp4
+local_client_python_script=client/client.py
+
+
+ssh -t $remote_server_user@$remote_server_ip "python3 $remote_python_script $remote_server_ip $local_client_transfer_file_path"
+
+python3 $local_client_python_script "$remote_server_ip $local_client_transfer_file_path"
