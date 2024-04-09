b133bf5b8e4628d6db1686ee287e77fbbb50df17
Ray Luberto <rluberto15@gmail.com>
2024-04-09 13:43:42 -0400

    remove data file

--- client/data.py
+++
@@ -1,9 +0,0 @@
-# The client data file is responsible for processing the timestamp data that is sent from the client
-
-import sys
-
-# Read the data beign sent from the client
-data = sys.stdin.read()
-
-# Print the data
-print("Received data:", data)
