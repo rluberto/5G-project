a48c3b79eee4bccfca84bd5ddf3ffd9330291937
Ray Luberto <rluberto15@gmail.com>
2024-04-10 11:43:57 -0400

    edit bash script

--- client/run-benchtest.sh
+++ client/run-benchtest.sh
@@ -2,13 +2,11 @@
 
 remote_server_ip=
 remote_server_user=
-remote_server_python_script=Developer/5G-project/server/server.py
+remote_server_python_script=Lab/5G-project/server/server.py
 
 local_client_transfer_file_path=media/video.mp4
 local_client_python_script=client/client.py
 
-read -s -p "Enter SSH password: " ssh_password
-
-sshpass -p "$ssh_password" ssh $remote_server_user@$remote_server_ip "python3 $remote_python_script $remote_server_ip $local_client_transfer_file_path"
+pid=S(ssh $remote_server_user@$remote_server_ip "nohup python3 $remote_server_python_script $remote_server_ip > /dev/null 2>&1 & echo \S!")
 
 python3 $local_client_python_script "$remote_server_ip $local_client_transfer_file_path"
