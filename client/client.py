e6b80d01468d30c285df69191ef1e175e60b6af5
Ray Luberto <rluberto15@gmail.com>
2024-04-10 02:18:24 -0400

    allow for any file or file extension to be transferred

--- client/client.py
+++ client/client.py
@@ -6,16 +6,17 @@ import sys
 # Define the server hostname or IP address
 SERVER_HOSTNAME = sys.argv[1]
 
-# Establish a socket connection for sending the image
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((SERVER_HOSTNAME, 8000))
-
 # Open the image file that is going to be transferred
 file_path = sys.argv[2]
 file = open(file_path, 'rb')
 
 # Create a send time array
 send_time_array = []
+file_metadata_array = [file_path.split('.')[-1]]
+
+# Establish a socket connection for sending the image
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER_HOSTNAME, 8000))
 
 # Write all of the image data to the server
 image_data = file.read(2048)
@@ -26,12 +27,13 @@ while image_data:
 file.close()
 client.close()
 
+
 # Establish a socket connection for sending benchmark data
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER_HOSTNAME, 9000))
 
-# Send the send time array to the server
-data = pickle.dumps(send_time_array)
-client.sendall(data)
-
+# Send the send time array and file metadata to the server
+send_time_data = pickle.dumps((send_time_array, file_metadata_array))
+client.sendall(send_time_data)
+# Close the client connection
 client.close()
--- server/server.py
+++ server/server.py
@@ -5,35 +5,32 @@ import pickle
 import time
 import json
 import sys
+import os
 
 # Define the server hostname or IP address
 SERVER_HOSTNAME = sys.argv[1]
 
-# Get the client file path to determine the file extension
-client_file_path = sys.argv[2]
-client_file_extension = client_file_path.split(".")[1]
-
 receive_time_array = []
 sent_time_array = []
+file_metadata = []
 transferred_data_length_array = []
 rta_processing_done = False
 sta_processing_done = False
-ready_to_stop_server = False
 file_random_number = ''
 
 def handle_connection(client_socket, port):
     global receive_time_array
     global sent_time_array
+    global file_metadata
     global rta_processing_done
     global sta_processing_done
     global file_random_number
-    global ready_to_stop_server
 
     # Transfer the image from the client to the server
     if port == 8000: # Used for receiving images
         print("Received connection on port 8000")
         file_random_number = str(random.randint(100000, 999999))
-        file = open('media/'+file_random_number+'_media.'+client_file_extension, "wb")
+        file = open('media/'+file_random_number+'_media', "wb")
         receive_time_array.clear()
         transferred_data_length_array.clear()
         image_chunk = client_socket.recv(2048, socket.MSG_WAITALL)
@@ -52,23 +49,33 @@ def handle_connection(client_socket, port):
     elif port == 9000: # Used for getting benchmark data
         print("Received connection on port 9000")
         sent_time_array.clear()
+        file_metadata.clear()
+        # Use pickle to load the data from the client
         data = b""
         while True:
             packet = client_socket.recv(4096)
             if not packet: break
             data += packet
-        sent_time_array = pickle.loads(data)
+        pickle_data = pickle.loads(data)
+        # Unpack the send_time_array and file metedata from the pickle data
+        sent_time_array = pickle_data[0]
+        file_metadata = pickle_data[1]
         sta_processing_done = True
 
     # Perform calculations based on the send time and receive time data
-    if rta_processing_done and sta_processing_done: # Calculate latency after both send and received arrays are complete
+    if rta_processing_done and sta_processing_done:
+        # After the image has been transferred and the image metadata is received, rename the file to include the file extension
+        file_extension = file_metadata[0]
+        os.rename('media/'+file_random_number+'_media', 'media/'+file_random_number+'_media.'+file_extension)
+        #Create an empty latency and bandwidth array
         latency_array = []
         bandwidth_array = []
+        # Calculate the latency and bandwidth for "data chunk" that was transferred
         for i in range(len(receive_time_array)):
             latency = receive_time_array[i] - sent_time_array[i]
             latency_array.append(latency)
             bandwidth_array.append(transferred_data_length_array[i] / latency)
-        # Print the benchmark arrays to a json file
+        # Collect all of the benchmark data arrays
         benchmark_data = {
             "transferred_data_length_array": transferred_data_length_array,
             "sent_time_array": sent_time_array,
@@ -76,12 +83,11 @@ def handle_connection(client_socket, port):
             "latency_array": latency_array,
             "bandwidth_array": bandwidth_array
         }
+        # Save the benchmark data to a JSON file
         with open('benchmark-data/'+file_random_number+'_benchmark.json', 'w') as json_file:
             json.dump(benchmark_data, json_file)
         rta_processing_done = False
         sta_processing_done = False
-        ready_to_stop_server = True
-
     client_socket.close()
 
 
@@ -92,7 +98,7 @@ def start_server(port):
     server_socket.listen(5)
     print(f"Listening on port {port}...")
 
-    while ready_to_stop_server == False:
+    while True:
         client_socket, address = server_socket.accept()
         print(f"Accepted connection from {address[0]}:{address[1]} on port {port}")
         threading.Thread(target=handle_connection, args=(client_socket, port)).start()
