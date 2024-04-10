2e5fb142bb61889d24331ef45f31be76b8b7ff87
Ray Luberto <rluberto15@gmail.com>
2024-04-10 12:15:08 -0400

    Update run-benchtest.sh
    update bash script

--- client/run-benchtest.sh
+++ client/run-benchtest.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+!/bin/bash
 
 remote_server_ip=
 remote_server_user=
-remote_server_python_script=Lab/5G-project/server/server.py
+remote_server_python_script=/home/rluberto/Lab/5G-project/server/server.py
 
 local_client_transfer_file_path=media/video.mp4
-local_client_python_script=client/client.py
+local_client_python_script=/home/rock/5G-project/client/client.py
 
-pid=S(ssh $remote_server_user@$remote_server_ip "nohup python3 $remote_server_python_script $remote_server_ip > /dev/null 2>&1 & echo \S!")
+pid=$(ssh $remote_server_user@$remote_server_ip "nohup python3 $remote_server_python_script >
 
-python3 $local_client_python_script "$remote_server_ip $local_client_transfer_file_path"
+python3 $local_client_python_script $remote_server_ip $local_client_transfer_file_path
