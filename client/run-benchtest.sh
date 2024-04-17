6347f4a9c0a79f002bc34da2c1f42b5de4e61f0d
Ray Luberto <rluberto15@gmail.com>
2024-04-17 11:52:01 -0400

    update bash script

--- client/run-benchtest.sh
+++ client/run-benchtest.sh
@@ -1,12 +1,22 @@
-!/bin/bash
+#!/bin/bash
 
-remote_server_ip=
-remote_server_user=
-remote_server_python_script=/home/rluberto/Lab/5G-project/server/server.py
+remote_server_ip=[ip address]
+remote_server_user=[username]
 
-local_client_transfer_file_path=media/video.mp4
-local_client_python_script=/home/rock/5G-project/client/client.py
+remote_server_python_script=/home/[username]/5G-project/server/server.py
+remote_server_output_file_path=/home/[username]/5G-project/server/media
+local_client_transfer_file_path=/home/[username]/5G-project/client/media/image.jpg
+local_client_python_script=/home/[username]/5G-project/client/client.py
 
-pid=$(ssh $remote_server_user@$remote_server_ip "nohup python3 $remote_server_python_script >
+
+TS=$(date +%s)
+benchmark_name="_benchmark.json"
+out_file="${TS}${benchmark_name}"
+
+ssh $remote_server_user@$remote_server_ip "nohup python3 $remote_server_python_script $remote_server_ip $TS > /home/[username]/server_out.txt 2>%1 &"
+sleep 1
 
 python3 $local_client_python_script $remote_server_ip $local_client_transfer_file_path
+
+cd /home/rock/benchmark_output
+scp [username]@$remote_server_ip:/home/[username]/5G-project/server/benchmark-data/$out_file ./
