e5a66d8df4bf83d05c8408fed8eadc56d4a3fda1
Ray Luberto <rluberto15@gmail.com>
2024-04-16 22:49:37 -0400

    perform unit conversions on server side

--- server/server.py
+++ server/server.py
@@ -11,18 +11,18 @@ import select
 # Define the server hostname or IP address
 SERVER_HOSTNAME = sys.argv[1]
 
-receive_time_array = []
-sent_time_array = []
+receive_time_array_ns = []
+sent_time_array_ns = []
 file_metadata = []
-transferred_data_length_array = []
+transferred_data_length_array_bytes = []
 receive_data_ready = False
 send_data_ready = False
 file_number = ''
 ready_to_stop_server = False
 
 def handle_connection(client_socket, port):
-    global receive_time_array
-    global sent_time_array
+    global receive_time_array_ns
+    global sent_time_array_ns
     global file_metadata
     global receive_data_ready
     global send_data_ready
@@ -34,24 +34,24 @@ def handle_connection(client_socket, port):
         print("Received connection on port 8000")
         file_number = sys.argv[2]
         file = open('media/'+file_number+'_media', "wb")
-        receive_time_array.clear()
-        transferred_data_length_array.clear()
+        receive_time_array_ns.clear()
+        transferred_data_length_array_bytes.clear()
         image_chunk = client_socket.recv(2048, socket.MSG_WAITALL)
-        receive_time_array.append(time.time_ns())
-        transferred_data_length_array.append(image_chunk.__len__())
+        receive_time_array_ns.append(time.time_ns())
+        transferred_data_length_array_bytes.append(image_chunk.__len__())
         while image_chunk:
             file.write(image_chunk)
             image_chunk = client_socket.recv(2048, socket.MSG_WAITALL)
             if image_chunk.__len__() != 0:
-                receive_time_array.append(time.time_ns())
-                transferred_data_length_array.append(image_chunk.__len__())
+                receive_time_array_ns.append(time.time_ns())
+                transferred_data_length_array_bytes.append(image_chunk.__len__())
         file.close()
         receive_data_ready = True
 
     # Transfer the send time data from the client to the server
     elif port == 9000: # Used for getting benchmark data
         print("Received connection on port 9000")
-        sent_time_array.clear()
+        sent_time_array_ns.clear()
         file_metadata.clear()
         # Use pickle to load the data from the client
         data = b""
@@ -61,7 +61,7 @@ def handle_connection(client_socket, port):
             data += packet
         pickle_data = pickle.loads(data)
         # Unpack the send_time_array and file metedata from the pickle data
-        sent_time_array = pickle_data[0]
+        sent_time_array_ns = pickle_data[0]
         file_metadata = pickle_data[1]
         send_data_ready = True
 
@@ -70,22 +70,57 @@ def handle_connection(client_socket, port):
         # After the image has been transferred and the image metadata is received, rename the file to include the file extension
         file_extension = str(file_metadata[0])
         os.rename('media/'+file_number+'_media', 'media/'+file_number+'_media.'+file_extension)
-        #Create an empty latency and bandwidth array
-        latency_array = []
-        bandwidth_array = []
+        # Create empty arrays for send time, receive time, adn transferred data length
+        receive_time_array_ms = []
+        send_time_array_ms = []
+        receive_time_array_seconds = []
+        send_time_array_seconds = []
+        transferred_data_length_array_bits = []
+        # Create empty arrays for latency and bandwidth
+        latency_array_ns = []
+        latency_array_ms = []
+        latency_array_seconds = []
+        bandwidth_array_bits_per_second = []
         # Calculate the latency and bandwidth for "data chunk" that was transferred
-        if(len(receive_time_array) == len(sent_time_array)):
-            for i in range(len(receive_time_array)):
-                latency = receive_time_array[i] - sent_time_array[i]
-                latency_array.append(latency)
-                bandwidth_array.append(transferred_data_length_array[i] / latency)
+        if(len(receive_time_array_ns) == len(sent_time_array_ns)):
+            # Convert units for time and transferred data length
+            for i in range(len(receive_time_array_ns)):
+                send_time_array_ms.append(sent_time_array_ns[i] / 1e6)
+                receive_time_array_ms.append(receive_time_array_ns[i] / 1e6)
+                send_time_array_seconds.append(sent_time_array_ns[i] / 1e9)
+                receive_time_array_seconds.append(receive_time_array_ns[i] / 1e9)
+                #Convert the transferred data length from bytes to bits
+                transferred_data_length_array_bits.append(transferred_data_length_array_bytes[i] * 8)
+            # Calculate the latency and bandwidth for each "data chunk" that was transferred
+            for i in range(len(receive_time_array_ns)):
+                # Calculae the latency in nanoseconds, milliseconds, and seconds
+                latency_ns = receive_time_array_ns[i] - sent_time_array_ns[i]
+                latency_ms = receive_time_array_ms[i] - send_time_array_ms[i]
+                latency_seconds = receive_time_array_seconds[i] - send_time_array_seconds[i]
+                # Append the latency in nanoseconds, milliseconds, and seconds to the latency arrays
+                latency_array_ns.append(latency_ns)
+                latency_array_ms.append(latency_ms)
+                latency_array_seconds.append(latency_seconds)
+                # Append the bandwidth in bits per second to the bandwidth array
+                bandwidth_array_bits_per_second.append(transferred_data_length_array_bits[i] / latency_seconds)
         # Collect all of the benchmark data arrays
         benchmark_data = {
-            "transferred_data_length_array": transferred_data_length_array,
-            "sent_time_array": sent_time_array,
-            "receive_time_array": receive_time_array,
-            "latency_array": latency_array,
-            "bandwidth_array": bandwidth_array
+            # Transferred data length
+            "transferred_data_length_array_bytes": transferred_data_length_array_bytes,
+            "transferred_data_length_array_bits": transferred_data_length_array_bits,
+            # Sent and received time
+            "sent_time_array_ns": sent_time_array_ns,
+            "receive_time_array_ns": receive_time_array_ns,
+            "send_time_array_ms": send_time_array_ms,
+            "receive_time_array_ms": receive_time_array_ms,
+            "send_time_array_seconds": send_time_array_seconds,
+            "receive_time_array_seconds": receive_time_array_seconds,
+            # Latency
+            "latency_array_ns": latency_array_ns,
+            "latency_array_ms": latency_array_ms,
+            "latency_array_seconds": latency_array_seconds,
+            # Bandwidth
+            "bandwidth_array_bits_per_second": bandwidth_array_bits_per_second
         }
         # Save the benchmark data to a JSON file
         with open('benchmark-data/'+file_number+'_benchmark.json', 'w') as json_file:
