aba88f06645ccb39838da359d41a6a2dcbf80daf
Ray Luberto <rluberto15@gmail.com>
2024-04-17 11:43:45 -0400

    update benchmark plots

--- server/benchmark-plotter.py
+++ server/benchmark-plotter.py
@@ -6,42 +6,77 @@ import sys
 # Use command line arguement to get benchmark file number
 benchmark_file_number = sys.argv[1]
 
+# Load data from json file
 data_file_path = 'benchmark-data/'+str(benchmark_file_number)+'_benchmark.json'
 data = json.load(open(data_file_path))
 
-sent_time_array = data['sent_time_array']
-receive_time_array = data['receive_time_array']
-latency_array = data['latency_array']
-bandwidth_array = data['bandwidth_array']
-transferred_data_length_array = data['transferred_data_length_array']
+# Extract data from json file
+transferred_data_length_array_bits = data["transferred_data_length_array_bits"]
+send_time_array_ns = data["send_time_array_ns"]
+receive_time_array_ns = data["receive_time_array_ns"]
 
-xpoints = np.array(sent_time_array)
-ypoints = np.array(latency_array)
+# Calculate latency
+latency_array_ns = []
+for i in range(len(send_time_array_ns)):
+    latency_array_ns.append(receive_time_array_ns[i] - send_time_array_ns[i])
 
-plt.xlabel('Bytes Sent (x 2048)')
-plt.ylabel('Latency (Milliseconds)')
-plt.title('Latency vs Bytes Sent')
-plt.plot(ypoints*1000)
+# Convert latency array from nanoseconds to milliseconds
+latency_array_ms = []
+for i in range(len(latency_array_ns)):
+    latency_array_ms.append(latency_array_ns[i] / 1000000)
+
+# Convert latency array from nanoseconds to seconds
+latency_array_seconds = []
+for i in range(len(latency_array_ns)):
+    latency_array_seconds.append(latency_array_ns[i] / 1000000000)
+
+# Calculate bandwidth bits/second
+bandwidth_array_bits_per_second = []
+for i in range(len(transferred_data_length_array_bits)):
+    bandwidth_array_bits_per_second.append(transferred_data_length_array_bits[i] / latency_array_seconds[i])
+
+# Calculate the running total of bits transferred
+total_bits_transferred = []
+for i in range(len(transferred_data_length_array_bits)):
+    total_bits_transferred.append(sum(transferred_data_length_array_bits[:i+1]))
+
+# Convert the receive time array from nanoseconds to milliseconds
+receive_time_array_ms = []
+for i in range(len(receive_time_array_ns)):
+    receive_time_array_ms.append(receive_time_array_ns[i] / 1000000)
+
+# Plot the Latecy vs Total Bits Transferred
+xpoints = np.array(total_bits_transferred)
+ypoints = np.array(latency_array_ms)
+plt.xlabel('Total Bits Transferred')
+plt.ylabel('Latency (milliseconds)')
+plt.title('Latency vs Total Bits Transferred')
+plt.plot(xpoints, ypoints)
+plt.gca().get_xaxis().get_major_formatter().set_scientific(False)
+plt.gca().get_yaxis().get_major_formatter().set_scientific(False)
+plt.grid()
 plt.show()
 
-xpoints = np.array(sent_time_array)
-ypoints = np.array(bandwidth_array)
-
-plt.xlabel('Bytes Sent (x 2048)')
-plt.ylabel('Bandwidth (Bits/Second)')
-plt.title('Bandwidth vs Bytes Sent')
-plt.plot(ypoints*8)
+# Plot the Bandwidth vs Total Bits Transferred
+xpoints = np.array(total_bits_transferred)
+ypoints = np.array(bandwidth_array_bits_per_second)
+plt.xlabel('Total Bits Transferred')
+plt.ylabel('Bandwidth (Bits per Second)')
+plt.title('Bandwidth vs Total Bits Transferred')
+plt.plot(xpoints, ypoints)
+plt.gca().get_xaxis().get_major_formatter().set_scientific(False)
+plt.gca().get_yaxis().get_major_formatter().set_scientific(False)
+plt.grid()
 plt.show()
 
-total_bytes_transferred_array = []
-for i in range(len(transferred_data_length_array)):
-    total_bytes_transferred_array.append(sum(transferred_data_length_array[:i+1]))
-
-xpoints = np.array(receive_time_array)
-ypoints = np.array(total_bytes_transferred_array)
-
-plt.ylabel('Total Bytes Transferred')
-plt.xlabel('Receive Time (Milliseconds)')
-plt.title('Total Bytes Transferred vs Receive Time')
-plt.plot((xpoints-receive_time_array[0])*1000, ypoints, marker='o')
+# Plot the Total Bits Transferred vs Elapsed Receive Time
+xpoints = np.array(receive_time_array_ms)
+ypoints = np.array(total_bits_transferred)
+plt.ylabel('Total Bits Transferred')
+plt.xlabel('Elapsed Receive Time (Milliseconds)')
+plt.title('Total Bits Transferred vs Elapsed Receive Time')
+plt.plot(xpoints-receive_time_array_ms[0], ypoints)
+plt.gca().get_xaxis().get_major_formatter().set_scientific(False)
+plt.gca().get_yaxis().get_major_formatter().set_scientific(False)
+plt.grid()
 plt.show()
